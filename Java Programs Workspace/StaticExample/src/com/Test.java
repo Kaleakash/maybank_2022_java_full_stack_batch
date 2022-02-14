70604883f789bf086cc2f0dfc90b04c4e7fc96bc
akash <akash300383@gmail.com>
2022-02-14 08:28:58 +0530

    done

--- Java Programs Workspace/StaticExample/src/com/Test.java
+++ Java Programs Workspace/StaticExample/src/com/Test.java
@@ -2,7 +2,6 @@ package com;
 class Abc {
 			int a;				// instance variable 
 			static int b;		// static variable 
-			
 			void dis1() {
 				System.out.println("Non static or instance method");
 				System.out.println("a "+a);
@@ -10,18 +9,20 @@ class Abc {
 			}
 			static void dis2() {
 				System.out.println("Static method");
-				//System.out.println("a "+a);
+				Abc obj = new Abc();
+				System.out.println("a "+obj.a);
 				System.out.println("b "+b);
 			}
 }
 public class Test {
 	public static void main(String[] args) {
+		Abc.dis2();
 		Abc obj = new Abc();
 		System.out.println(obj.a);					// through object 
-		System.out.println(Abc.b);					// through class name
-		System.out.println(obj.b);                // through object static variable 
-		obj.dis1();      // calling non static method 
-		Abc.dis2();     // calling static method 
-		obj.dis2();    // calling static method through object. 
+//		System.out.println(Abc.b);					// through class name
+//		System.out.println(obj.b);                // through object static variable 
+//		obj.dis1();      // calling non static method 
+//		Abc.dis2();     // calling static method 
+//		obj.dis2();    // calling static method through object. 
 	}
 }
