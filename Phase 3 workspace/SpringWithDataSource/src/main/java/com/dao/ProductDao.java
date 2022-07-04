81f86d47444ba58b5cf419d2e88199de90e40d57
akash <akash300383@gmail.com>
2022-07-04 08:35:54 +0530

    done

--- Phase 3 workspace/SpringWithDataSource/src/main/java/com/dao/ProductDao.java
+++ Phase 3 workspace/SpringWithDataSource/src/main/java/com/dao/ProductDao.java
@@ -2,6 +2,9 @@ package com.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.sql.DataSource;
 
@@ -18,6 +21,7 @@ public class ProductDao {
 	@Autowired
 	DataSource ds;			// pull the database connection from xml file 
 	
+	// pure Jdbc code 
 	public int storeEmployee(Product product) {
 		try {
 			Connection con = ds.getConnection();			// we will the get the connection. 
@@ -31,5 +35,25 @@ public class ProductDao {
 			System.out.println(e);
 			return 0;
 		}
+	}
+	
+	public List<Product> getAllProduct() {
+		List<Product> listOfProduct = new ArrayList<Product>();
+		try {
+			Connection con = ds.getConnection();
+			PreparedStatement pstmt = con.prepareStatement("select * from product");
+			ResultSet rs = pstmt.executeQuery();
+			while(rs.next()) {
+				Product p = new Product();
+				p.setPid(rs.getInt(1));
+				p.setPname(rs.getString(2));
+				p.setPrice(rs.getFloat(3));
+				listOfProduct.add(p);
+			}
+			return listOfProduct;
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return null;
 	}
 }
--- Phase 3 workspace/SpringWithDataSource/src/main/java/com/sevice/ProductService.java
+++ Phase 3 workspace/SpringWithDataSource/src/main/java/com/sevice/ProductService.java
@@ -1,16 +1,23 @@
 package com.sevice;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bean.Product;
 import com.dao.ProductDao;
+import com.dao.ProductDaoJdbcTemplate;
 
 @Service						// id is productService
 public class ProductService {
 
 	@Autowired
 	ProductDao productDao;					// pull the object from container. 
+	
+	
+	@Autowired
+	ProductDaoJdbcTemplate productDaoJdbcTemplate;			// di for ProductDaoJdbcTemplate 
 	
 	public String storeProduct(Product product) {
 		if(product.getPrice()<1000) {
@@ -20,5 +27,21 @@ public class ProductService {
 		}else {
 			return "Product didn't store";
 		}
+	}
+	
+	public String storeProductUsingJdbcTemplate(Product product) {
+		if(productDaoJdbcTemplate.storeProduct(product)>0) {
+			return "Product Details stored successfully";
+		}else {
+			return "Product didn't store";
+		}
+	}
+	
+	public List<Product> getAllProduct() {
+		return productDao.getAllProduct();
+	}
+	
+	public List<Product> getAllProductUsingJdbcTemplate() {
+		return productDaoJdbcTemplate.getAllProduct();
 	}
 }
